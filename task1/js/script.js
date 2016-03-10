31c7b3d18ec343b085971d18ed6e70908fd5b9c5
KarlBao <karl930219@gmail.com>
2016-03-10 20:35:30 +0800

    Merge remote-tracking branch 'Baidu-IFE-Go/master'
    # Conflicts:
    #	task1/index.html
    #	task1/js/script.js

--- task1/js/script.js
+++ task1/js/script.js
@@ -5,9 +5,62 @@ $(document).ready(function() {
 	/*********** 乔震 ************/
 
 	/*********** 鲍鲍 ************/
+	(function($) {
 
+		$.fn.fadeSlider = function(options) {
+
+			// 默认参数
+			var defaults = {
+				fadeSpeed: 500,		// 淡入淡出速度
+				displayDuration: 5000		// 播放停留时间
+			}
+
+			var $slides = [],
+				$nav,
+				$slider = this,
+				settings = $.extend({},defaults,options),
+				namespace = 'fs-',
+				methods = {};
+
+			// 私有方法
+			methods = {
+
+				// 初始化
+				init: function() {
+					$slides = $slider.find('slides li');
+					$nav = methods.createNav();
+				},
+
+				// 创建DOM
+				createDOM: function() {
+
+					$slides = $slides.map(function(item, index) {
+						var $slide = item;
+						$slide.addClass('slide');
+						return $slide;
+					});
+					$slider.append($nav);
+				},
+
+				// 创建索引
+				createNav: function() {
+					var $nav,
+						$li = [];
+
+					for(var i=0; i<$slides.length; i++) {
+						$li.push($('<li><a href="#"></a></li>'));
+					}
+
+					$nav = $('<ol class="'namespace+'nav">'+$li.join('')+'</ol>');
+					return $nav;
+				}
+			};
+
+			
+		}
+	})(jQuery);
 	/*********** 小鑫鑫 **********/
 
 	/*********** 岳谱 ************/
-	
+
 })
