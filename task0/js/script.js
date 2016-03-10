1c0dc627c595757e7446bd978b28e02d05925ca3
Daniel <a13761893922@163.com>
2016-03-10 18:00:10 +0800

    top60%

--- task0/js/script.js
+++ task0/js/script.js
@@ -2,6 +2,19 @@ $(document).ready(function() {
 
 	/*********** 张磊磊 ***********/
 
+	$('.window').find('ul').hover(
+	  function(){
+	  $('.window').find('ul').animate({"top": "-=26px"}, 300);
+	  },
+	  function(){
+	  $('.window').find('ul').animate({"top": "-=-26px"}, 300);
+	});
+
+
+
+
+
+
 	/*********** 乔震 ************/
 
 	/*********** 鲍鲍 ************/
