afc9dbf5be56ff46e65aabfc4653ef6628d31e71
Daniel <597879387@qq.com>
2016-03-14 00:03:15 +0800

    top100%

--- task0/js/script.js
+++ task0/js/script.js
@@ -8,9 +8,21 @@ $(document).ready(function() {
 	  function(){
 	  $(this).stop(true, true).animate({"top": "-=-26px"}, 300);
 	});
-
-
-
+	$('.window > ul').eq(1).on('click',function(){
+		$('body,html').animate({scrollTop:651},500);
+	});
+	$('.window > ul').eq(2).on('click',function(){
+		$('body,html').animate({scrollTop:1551},500);
+	});
+	$('.window > ul').eq(3).on('click',function(){
+		$('body,html').animate({scrollTop:2450},500);
+	});
+	$('.window > ul').eq(4).on('click',function(){
+		$('body,html').animate({scrollTop:2970},500);
+	});
+	$('.banner').find('span').on('click',function(){
+		$('body,html').animate({scrollTop:651},500);
+	});
 
 
 
