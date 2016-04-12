6ca2c374cef3c420463c9b7c9e6ef3f19421031f
2b2b2b2b <a13761839322@163.com>
2016-04-12 18:13:44 +0800

    task21

--- task2/21/张磊磊/JS/task20.js
+++ task2/21/张磊磊/JS/task20.js
@@ -1,33 +1,39 @@
-window.onload = function(){
-	(function(){
-		inputList = [];
-		function init ($tagInput){
-			$tagInput.each(function(){
-				var $cal = $(this);
-				if ($cal.hasClass('init')){
-					return;
-				}
-				inputList.push(new tagInput($cal));
-				$cal.addClass('init');
-			})
-		};
-		function getList(){
-			return inputList;
-		}
-		function tagInput($tag){
-			this.$tag = $tag;
-			this.$input = $tag.find('.input');
-			this.$result = $tag.find('.result');
-			var this.listArr = [];
-		};
-		function render(){
-			var this.str = "";
-			for (var i = 0; i < this.listArr.length; i++) {
-				this.str += '<div>' + this.listArr[i] + '</div>'; 
-			}
-			this.result.innerHTML = this.str ;
-		};
-		
 
-	})()		
-}
+	function init($tagInput){
+			this.$tagInput = $tagInput,
+			this.$result = $tagInput.find('.result');
+			this.$input = $tagInput.find('.input');
+			this.listArr  = [];
+		}
+		init.prototype	 = {
+			render:function(){
+				var str = "";
+				for (var i = 0; i < this.listArr.length; i++) {
+					str += '<div>' + this.listArr[i] + '</div>'; 
+				}
+				console.log(this.listArr);
+				console.log(str);
+				this.$result.append(str);				
+			},
+			In:function(){
+				if(!this.$input.val()){
+					alert("请输入");
+					return false;
+				}
+				else if (/[^\n\r\s 、,，0-9A-Za-z\u4e00-\u9fa5]/.test(this.$input.val())){
+					alert("请输入数字");
+					return false;
+				}
+				var splitArr = this.$input.val().trim().split(/[\s\r\n、,，]+/);
+				//操作数组 
+				this.listArr = splitArr.concat(this.listArr);				
+			},
+		}
+	var tag = new init($('.tag-input'));
+	var hobby = new init($('.hobby-input'));
+	var $hobbyBtn = $('.confirm');
+	//绑定事件
+	$hobbyBtn.on('click',function(){
+		hobby.In();
+		hobby.render();
+	})
