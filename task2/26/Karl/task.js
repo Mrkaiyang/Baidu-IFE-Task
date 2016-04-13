63b2e073e4c63bab51ea9e960173eef329ca52a5
KarlBao <karl930219@gmail.com>
2016-04-14 00:28:52 +0800

    change tab to 4 spaces

--- task2/26/Karl/task.js
+++ task2/26/Karl/task.js
@@ -11,44 +11,44 @@ var spaceshipArr = [];
 //飞船
 var SpaceShip = function(id) {
 
-	var $spaceship;
-	var energy = 100; 	//总能量
-	var consumption = 5;//飞船单位时间(s)能耗
+    var $spaceship;
+    var energy = 100;   //总能量
+    var consumption = 5;//飞船单位时间(s)能耗
     var chargeSpeed = 2;//飞船能源系统充电速率
-	var speed = 1;		//飞船线速度
-	var orbitIndex = Math.floor(Math.random()*orbits.length);		//所属轨道
-	var fps = 60; //动画播放帧率
+    var speed = 1;      //飞船线速度
+    var orbitIndex = Math.floor(Math.random()*orbits.length);       //所属轨道
+    var fps = 60; //动画播放帧率
 
-	var status = false;	//飞船状态，true为飞行中
-	var radius = orbits[orbitIndex];	//轨道半径
-	var angularSpeed = 360*speed/(2*radius*Math.PI);	//飞船角速度
-	var curAngular = 360*Math.random(); //飞船当前角度
-	var period = 1000/fps;	//动画播放周期
+    var status = false; //飞船状态，true为飞行中
+    var radius = orbits[orbitIndex];    //轨道半径
+    var angularSpeed = 360*speed/(2*radius*Math.PI);    //飞船角速度
+    var curAngular = 360*Math.random(); //飞船当前角度
+    var period = 1000/fps;  //动画播放周期
     var maxEnergy = 100;
 
-	// 飞船动力系统
-	var engine = {
+    // 飞船动力系统
+    var engine = {
 
-		// 初始化动力系统
-		init: function() {
-			
-		},
-		// 启动
-		start: function() {
-			status = true;
+        // 初始化动力系统
+        init: function() {
+            
+        },
+        // 启动
+        start: function() {
+            status = true;
             log.out(id+'号飞船启动');
-		},
-		// 停止
-		stop: function() {
-			status = false;
+        },
+        // 停止
+        stop: function() {
+            status = false;
             log.out(id+'号飞船已停止');
-		},
+        },
         // 消耗能源
         consumeEnergy: function() {
             if(energy>0) { energy-=consumption/fps; }
             else { engine.stop(); }
         }
-	};
+    };
 
     // 飞船能源系统
     var energySystem = {
@@ -56,17 +56,17 @@ var SpaceShip = function(id) {
             if(energy<maxEnergy) { energy+=chargeSpeed/fps; }
         }
     }
-	// 创建飞船
-	var init = function() {
-		$spaceship = $('<div id="spaceship'+id+'" class="spaceship"></div>');
-		render();
-		$spaceship.css({ 
-			transform: 'translateY(-'+radius+'px) rotate(0)',
-			transformOrigin: 'center '+(20+radius)+'px'
-		});
-		engine.init();
+    // 创建飞船
+    var init = function() {
+        $spaceship = $('<div id="spaceship'+id+'" class="spaceship"></div>');
+        render();
+        $spaceship.css({ 
+            transform: 'translateY(-'+radius+'px) rotate(0)',
+            transformOrigin: 'center '+(20+radius)+'px'
+        });
+        engine.init();
         log.out(id+'号飞船已发送至'+(orbitIndex+1)+'号轨道');
-	}
+    }
 
     // 销毁飞船
     var destroy = function() {
@@ -78,24 +78,24 @@ var SpaceShip = function(id) {
         log.out(id+'号飞船已销毁')
     }
 
-	//绘制飞船
-	var render = function() {
+    //绘制飞船
+    var render = function() {
         $spaceship.append('<div class="energy-progress"></div>');
         $spaceship.append('<div class="energy-text"><label>'+id+'号 - </label><span class="energy">'+energy+'</span><span>%</span></div>');
-		$spaceship.appendTo('#space');
-	}
+        $spaceship.appendTo('#space');
+    }
 
-	// 逐帧渲染飞船动作
-	var frameAnimation = setInterval(function(){
+    // 逐帧渲染飞船动作
+    var frameAnimation = setInterval(function(){
         // 任何状态下
         energySystem.chargeEnergy();
-		// 飞行状态
-		if(status) {
+        // 飞行状态
+        if(status) {
             // 飞船轨迹变化
-			curAngular+=angularSpeed;
+            curAngular+=angularSpeed;
             // 飞船能源变化
             engine.consumeEnergy();
-		}
+        }
         // 渲染
         $spaceship.css({
             transform: 'translateY(-'+radius+'px) rotate('+curAngular%360+'deg)'
@@ -103,7 +103,7 @@ var SpaceShip = function(id) {
         $('.energy',$spaceship).text(parseInt(energy));
         $('.energy-progress',$spaceship).css('width',energy);
 
-	},period);
+    },period);
 
     // 获取飞船的jquery对象
     var getObj = function() {
@@ -126,28 +126,28 @@ var SpaceShip = function(id) {
 
     init();
 
-	// 返回公共接口
-	return {
-		id: id,   //飞船id
+    // 返回公共接口
+    return {
+        id: id,   //飞船id
         getObj: getObj,  //飞船的jquery对象
         receiveSignal: receiveSignal    //飞船接收指令
-	}
+    }
 }
 
 var Space = function() {
-	var init = function() {
-		//渲染轨道
-		for(var index in orbits) {
-			var radius = orbits[index];
-			var orbit = $('<div class="orbit"></div>');
-			orbit.css({
-				width: 2*radius,
-				height: 2*radius
-			});
-			orbit.appendTo('#space');
-		}
-	}
-	init();
+    var init = function() {
+        //渲染轨道
+        for(var index in orbits) {
+            var radius = orbits[index];
+            var orbit = $('<div class="orbit"></div>');
+            orbit.css({
+                width: 2*radius,
+                height: 2*radius
+            });
+            orbit.appendTo('#space');
+        }
+    }
+    init();
 }
 
 var Mediator = function() {
@@ -176,48 +176,48 @@ var Mediator = function() {
     }
 }
 var ControlPanel = function() {
-	var totalShips = 0;	//控制台所知的现有飞船总数（与实际未必相符）
+    var totalShips = 0; //控制台所知的现有飞船总数（与实际未必相符）
     var maxShips = 4;   //最大飞船数量
-	var id = 0;	//飞船id计数器，自动递增
-	// 发射新飞船
-	var launchShip = function() {
+    var id = 0; //飞船id计数器，自动递增
+    // 发射新飞船
+    var launchShip = function() {
         if(totalShips == maxShips) { log.out('飞船数量达到上限');return false; }
-		var spaceship = new SpaceShip(++id);
+        var spaceship = new SpaceShip(++id);
         spaceshipArr.push(spaceship);
-		createShipPanel(id);
-		// 将飞船对象与实体绑定
-		$('#spaceship'+spaceship.id).data('fn', spaceship);
+        createShipPanel(id);
+        // 将飞船对象与实体绑定
+        $('#spaceship'+spaceship.id).data('fn', spaceship);
         totalShips++;
-	}
-	/**
-	 * 创建对应的飞船控制面板
-	 * @param {spaceship} 面板对应的飞船id
-	 **/
-	var createShipPanel = function(id) {
-		var $shipPanel = $('<div class="ship-panel"><label>'+id+'号飞船</label><button class="start-btn">飞行</button><button class="stop-btn">停止</button><button class="destroy-btn">销毁</button></div>');
-		$shipPanel.appendTo('#ship-panel-container');
-		shipHandler($shipPanel, id);
-	}
-	/**
-	 * 控制台与飞船进行绑定
-	 * @param {$panel} 需要绑定的控制面板
-	 * @param {$spaceship} 需要绑定的飞船对象
-	 **/
-	var shipHandler = function($panel,id) {
+    }
+    /**
+     * 创建对应的飞船控制面板
+     * @param {spaceship} 面板对应的飞船id
+     **/
+    var createShipPanel = function(id) {
+        var $shipPanel = $('<div class="ship-panel"><label>'+id+'号飞船</label><button class="start-btn">飞行</button><button class="stop-btn">停止</button><button class="destroy-btn">销毁</button></div>');
+        $shipPanel.appendTo('#ship-panel-container');
+        shipHandler($shipPanel, id);
+    }
+    /**
+     * 控制台与飞船进行绑定
+     * @param {$panel} 需要绑定的控制面板
+     * @param {$spaceship} 需要绑定的飞船对象
+     **/
+    var shipHandler = function($panel,id) {
         $('.start-btn',$panel).on('click',function(){
             var cmd = {
                 id: id,
                 command: 'start'
             }
-			mediator.broadcast(cmd);
-		});
-		$('.stop-btn',$panel).on('click',function(){
-			var cmd = {
+            mediator.broadcast(cmd);
+        });
+        $('.stop-btn',$panel).on('click',function(){
+            var cmd = {
                 id: id,
                 command: 'stop'
             }
             mediator.broadcast(cmd);
-		});
+        });
         $('.destroy-btn',$panel).on('click',function(){
             var cmd = {
                 id: id,
@@ -227,11 +227,11 @@ var ControlPanel = function() {
             $panel.remove();
             totalShips--;
         })
-	}
-	// 公共方法
-	return {
-		launchShip: launchShip
-	}
+    }
+    // 公共方法
+    return {
+        launchShip: launchShip
+    }
 }
 
 var Log = function($panel) {
@@ -249,17 +249,17 @@ var Log = function($panel) {
 $(document).ready(function(){
 
     // 创建宇宙
-	space = new Space();
+    space = new Space();
 
     // 创建信号传输介质
     mediator = new Mediator();
 
     // 创建飞船控制台
-	controlPanel = new ControlPanel();
+    controlPanel = new ControlPanel();
 
     // 创建宇航日志
     log = new Log($('#log-panel'));
-	$('#launch-btn').on('click', function(event) {
-		controlPanel.launchShip();
-	});
+    $('#launch-btn').on('click', function(event) {
+        controlPanel.launchShip();
+    });
 })
